c5190f643dd181088c3e45b7fe2771a358c04a77
Andre Ray <andreray053@gmail.com>
2022-09-06 12:00:09 -0300

    adicionando métodos post, put e delete no controller

--- users/src/main/java/com/api/users/controller/UsersController.java
+++ users/src/main/java/com/api/users/controller/UsersController.java
@@ -4,9 +4,11 @@ package com.api.users.controller;
 import com.api.users.domain.model.Users;
 import com.api.users.domain.model.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +31,35 @@ public class UsersController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Users createUser(@Valid @RequestBody Users user) {
+        return usersRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@Valid @RequestBody Users user, @PathVariable Long id) {
+        if(!usersRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setId(id);
+
+        user = usersRepository.save(user);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@Valid @PathVariable Long id) {
+        if(!usersRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        usersRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
